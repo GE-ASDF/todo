25a976e5b918a1958a57d980cac7f097038b273d
Anderson Souza <andersonsouza007@live.com>
2023-10-14 17:39:01 -0300

    Alterando a obtenção das tarefas por usuário

--- src/api/constants/Users.js
+++ src/api/constants/Users.js
@@ -4,5 +4,6 @@ module.exports = {
     userAlreadyExistsMessage:"Já existe um usuário cadastrado com os dados informados.",
     emptyData: "Os dados estão vazios.",
     errorCreatingUser:"Ocorreu um erro na hora de criar um usuário.",
-    invalidUserOrPass:"O usuário ou senha digitados são inválidos. Verifique os dados e tente novamente!"
+    invalidUserOrPass:"O usuário ou senha digitados são inválidos. Verifique os dados e tente novamente!",
+    successMessage:"Registro cadastrado com sucesso!"
 }
